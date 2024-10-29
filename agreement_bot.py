ae67032a3980c1db5c68122567367897ea799932
Fatima8024 <fatimajaved8024@gmail.com>
2024-10-29 22:23:04 +0500

    Create agreement_bot.py

---
+++ agreement_bot.py
@@ -0,0 +1,11 @@
+Write a program which asks the user what their favorite animal is, and then always responds with "My favorite animal is also ___!" (the blank should be filled in with the user-inputted animal, of course).
+
+# Prompt the user for their favorite animal
+animal = input("What is your favorite animal? ")
+
+# Respond with the user's favorite animal
+print(f"My favorite animal is also {animal}!")
+
+Answer
+What is your favorite animal? ram
+My favorite animal is also ram!
