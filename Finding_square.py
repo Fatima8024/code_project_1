ff1d58e9d41f678ed8262449d1442544212f0a06
Fatima8024 <fatimajaved8024@gmail.com>
2024-11-01 19:57:46 +0500

    Create Finding_square.py

---
+++ Finding_square.py
@@ -0,0 +1,9 @@
+# Ask the user for a number and print its square (the product of the number times itself).
+
+num = float(input("Enter a number to find it's square: "))
+square = num * num
+print(f"The square of the {num} is {square}")
+
+Solution
+Enter a number to find it's square: 8
+The square of the 8.0 is 64.0
