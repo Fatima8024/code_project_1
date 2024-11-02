2ff157d8e40c53835c059154dad6e2a93ff4bede
Fatima8024 <fatimajaved8024@gmail.com>
2024-11-02 18:42:19 +0500

    Create 01_feet_to_inches.py

---
+++ 01_feet_to_inches.py
@@ -0,0 +1,12 @@
+#Convert feet to inches
+
+m1:float = float(input("Enter your height in feet: "))
+
+m2 :float = m1 * 12
+
+print(f"Your desired height in inches is: {m2}")
+
+Solution
+Enter your height in feet: 5.2
+Your desired height in inches is: 62.400000000000006
+
