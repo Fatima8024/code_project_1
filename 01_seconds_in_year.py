dd03230c880f825bad5b798a66d805d6bff04760
Fatima8024 <fatimajaved8024@gmail.com>
2024-11-03 07:53:12 +0500

    Create 01_seconds_in_year.py

---
+++ 01_seconds_in_year.py
@@ -0,0 +1,11 @@
+# Calculate the number of seconds in a year
+
+Day_per_year = 365
+Hour_per_day = 24
+Min_per_hour = 60
+Sec_per_min = 60
+
+print(f"The number of seconds in a year is {Day_per_year * Hour_per_day * Min_per_hour * Sec_per_min}")
+
+Solution
+The number of seconds in a year is 31536000
